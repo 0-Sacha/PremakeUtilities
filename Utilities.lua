576363ae0ce44e2a6481d3afd6a4bb883f3ede0b
Sacha <bellier.sacha2@gmail.com>
2023-02-26 19:22:35 +0100

    refa() : Change path for Utilities file

---
+++ Utilities.lua
@@ -0,0 +1,10 @@
+
+Solution = {}
+
+include("PremakeUtilities/Defines.lua")
+include("PremakeUtilities/Path.lua")
+include("PremakeUtilities/IncludeFunctions.lua")
+include("PremakeUtilities/EnvVars.lua")
+include("PremakeUtilities/Clean.lua")
+
+include("PremakeUtilities/Handler/Handler.lua")
