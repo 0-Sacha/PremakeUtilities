30b8ddb6fda209d0972b9d767264372bc1db6db7
BELLIER Sacha <bellier.sacha2@gmail.com>
2023-04-19 19:02:58 +0200

    fix(Project include) : fix the issue of visual studio not including the . folder

--- PremakeUtilities/IncludeFunctions.lua
+++ PremakeUtilities/IncludeFunctions.lua
@@ -7,6 +7,8 @@ Solution.ProjectsInfo = {}
 Solution.ProjectsInfo.Defines = {}
 Solution.ProjectsInfo.Includes = {}
 Solution.ProjectsInfo.Links = {}
+Solution.ProjectsInfo.ProjectDependencies = {}
+Solution.ProjectsInfo.HeaderOnly = {}
 Solution.ProjectsInfo.PlatformDefineName = {}
 
 Solution.IncludeProject = function(projectName)
@@ -39,10 +41,38 @@ Solution.IncludeProject = function(projectName)
             end
         end
     end
+
+    if Solution.ProjectsInfo.Links ~= nil then
+        if Solution.ProjectsInfo.Links[projectName] ~= nil then
+            for _, project_to_link in ipairs(Solution.ProjectsInfo.Links[projectName]) do
+                printf("Using the Solution.ProjectsInfo.Links is weird '%s'", project_to_link)
+                links (project_to_link)
+            end
+        end
+    end
+
+    if Solution.ProjectsInfo.ProjectDependencies ~= nil then
+        if Solution.ProjectsInfo.ProjectDependencies[projectName] ~= nil then
+            for _, project_dependencies in ipairs(Solution.ProjectsInfo.ProjectDependencies[projectName]) do
+                printf("Recusivelly add dependencies of '%s' -> '%s'", projectName, project_dependencies)
+                Solution.IncludeAndLinkProject(project_dependencies)
+            end
+        end
+    end
 end
 
 
 Solution.IncludeAndLinkProject = function(projectName)
     Solution.IncludeProject(projectName)
-    links (projectName)
+
+    if Solution.ProjectsInfo.HeaderOnly ~= nil then
+        if Solution.ProjectsInfo.HeaderOnly[projectName] == nil or Solution.ProjectsInfo.HeaderOnly[projectName] ~= true then
+            links (projectName)
+        end
+    end
+end
+
+Solution.Project = function(prefix)
+	includedirs { "." }
+	Solution.IncludeProject(prefix)
 end
